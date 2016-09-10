0623f40bf6ca16e1ff7f77d2a73a3d3f5ca6247b
Larry Muhlstein <larrymuhlstein@gmail.com>
2016-09-10 16:42:54 -0700

    Cleaned Definition

--- Server/api.py
+++ Server/api.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
 
 # create our little application :)
-app = Flask(__name__, static_folder='Client/static', template_folder='Client/template')
+app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 """app.config.update(dict(
@@ -17,38 +17,46 @@ app = Flask(__name__, static_folder='Client/static', template_folder='Client/tem
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)"""
 
 
-@app.route('/')
-def test():
-    return render_template('test.html')
-
 @app.route('/start_streaming')
 def start_streaming():
-
+    print("Starting Stream")
+    return
 
 @app.route('/stop_streaming')
 def stop_streaming():
+    print("Stopping Stream")
     stop_logging()
 
 
 @app.route('/pause_streaming')
-
-
-pause_logging()
+def pause_streaming():
+    print("Pausing Stream")
+    pause_logging()
 
 
 def send_distraction():
-
+    return
 
 def send_analytics():
+    return
 
 
 def start_logging():
+    return
 
 
 def stop_logging():
+    return
 
 
 def pause_logging():
+    return
 
 
 @app.teardown_appcontext
+def teardown():
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
