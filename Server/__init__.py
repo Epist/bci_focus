0ab88fd4f61aa466e9d772ae993cc54611614634
Larry Muhlstein <larrymuhlstein@gmail.com>
2016-09-10 17:35:17 -0700

    Fixed a few bugs Added pause toggling Began integrating openbci_control and api

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,1 +1,2 @@
 from api import api
+from openbci_control import OpenBCIAdapter
--- Server/api.py
+++ Server/api.py
@@ -1,10 +1,13 @@
 import os
 from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash
-
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from openbci_control import OpenBCIAdapter
 # create our little application :)
 app = Flask(__name__)
+BCI_instance = OpenBCIAdapter()
+
+
+
 
 # Load default config and override config from an environment variable
 """app.config.update(dict(
@@ -20,18 +23,35 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)"""
 @app.route('/start_streaming')
 def start_streaming():
     print("Starting Stream")
-    return
+    BCI_instance.start_streaming()
 
 @app.route('/stop_streaming')
 def stop_streaming():
     print("Stopping Stream")
+    BCI_instance.stop_streaming()
     stop_logging()
 
 
 @app.route('/pause_streaming')
 def pause_streaming():
     print("Pausing Stream")
-    pause_logging()
+    if BCI_instance.get_pause_state() == False:
+        BCI_instance.pause_streaming()
+        pause_logging()
+        return ("Paused")
+    else:
+        return ("Running")
+
+
+@app.route('/resume_streaming')
+def resume_streaming():
+    print("Resuming_Stream")
+    if BCI_instance.get_pause_state() == True:
+        BCI_instance.resume_streaming()
+        resume_logging()
+        return ("Running")
+    else:
+        return ("Paused")
 
 
 def send_distraction():
@@ -51,6 +71,9 @@ def stop_logging():
 
 def pause_logging():
     return
+
+
+def resume_logging():
 
 
 @app.teardown_appcontext
